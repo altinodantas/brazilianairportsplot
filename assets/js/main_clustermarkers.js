852c4a93fb1d9a68d7fc761601de991952e312a4
Altino Dantas <altinodantas@hotmail.com>
2021-08-13 19:11:28 -0300

    add marker cluster functionality

---
+++ assets/js/main_clustermarkers.js
@@ -0,0 +1,90 @@
+// Mapa
+const mapCenter = [-18.519074, -49.526367]
+const zoom = 6
+
+const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+const map = L.map('mapid', {
+  center: mapCenter,
+  zoom: zoom,
+  layers: [mainLayer],
+  zoomOffset: -1,
+  preferCanvas: true
+})
+
+// Icons
+const LeafIcon = L.Icon.extend({
+  options: {
+    // shadowUrl: 'assets/img/shadow.png',
+    iconSize: [22, 35], // size of the icon
+    shadowSize: [46, 12], // size of the shadow
+    iconAnchor: [11, 34], // point of the icon which will correspond to marker's location
+    shadowAnchor: [18, 9], // the same for the shadow
+    popupAnchor: [0, -35] // point from which the popup should open relative to the iconAnchor
+  }
+})
+
+const greenIcon = new LeafIcon({ iconUrl: 'assets/img/green.svg' })
+const redIcon = new LeafIcon({ iconUrl: 'assets/img/red.svg' })
+const yellowIcon = new LeafIcon({ iconUrl: 'assets/img/yellow.svg' })
+const blueIcon = new LeafIcon({ iconUrl: 'assets/img/azul.svg' })
+
+var markers = L.markerClusterGroup();
+    
+fetch("files/aerodromos_publicos.json")
+.then(response => response.json())
+.then(function(data) {
+
+  data.forEach(element => {
+    
+    const marker = L.marker([element.latitude,element.longitude], {icon: greenIcon})
+    marker.bindPopup(` <strong>${element.OACI} - ${element.NOME} </strong> <br/>Aeródromo público<br/> ${element.MUNICÍPIO}, ${element.UF} <br />lt:${element.latitude}, lg:${element.longitude}`)
+
+    markers.addLayer(marker);
+
+  });
+
+
+    map.addLayer(markers);
+    map.fitBounds(markers.getBounds());
+
+});
+    
+fetch("files/aerodromos_privados.json")
+  .then(response => response.json())
+  .then(function(data) {
+
+    data.forEach(element => {
+      
+      const marker = L.marker([element.LATGEOPOINT,element.LONGEOPOINT], {icon: yellowIcon})
+      marker.bindPopup(` <strong>${element.OACI} - ${element.Nome} </strong> <br/>Aeródromo privado <br/> ${element.Município}, ${element.UF} <br />lt:${element.LATGEOPOINT}, lg:${element.LONGEOPOINT}`)
+
+      markers.addLayer(marker);
+
+    });
+
+
+    map.addLayer(markers);
+    map.fitBounds(markers.getBounds());
+
+  });
+
+  fetch("files/helipontos.json")
+  .then(response => response.json())
+  .then(function(data) {
+
+    data.forEach(element => {
+      
+      const marker = L.marker([element.LATGEOPOINT,element.LONGEOPOINT], {icon: blueIcon})
+      marker.bindPopup(` <strong>${element.OACI} - ${element.Nome} </strong> <br/>Heliponto <br/> ${element.Município}, ${element.UF} <br />lt:${element.LATGEOPOINT}, lg:${element.LONGEOPOINT}`)
+
+      markers.addLayer(marker);
+
+    });
+  
+    map.addLayer(markers);
+    map.fitBounds(markers.getBounds());
+
+  });
